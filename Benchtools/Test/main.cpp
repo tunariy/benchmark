738cbbc1c66b5166b3cc5f1e595740b698194628
t <t.aryurek@student.utwente.nl>
2026-04-21 22:47:49 +0200

    builds on my machine

--- Benchtools/Test/main.cpp
+++ Benchtools/Test/main.cpp
@@ -1,31 +1,17 @@
+#include "Benchtools/Core/Time.hpp"
 #include <Benchtools/Core/FileStream.hpp>
-
-#include <Benchtools/Timers/Timers.hpp>
-
 #include <Benchtools/Logger/Logger.hpp>
+#include <Benchtools/Timers.hpp>
 
 #include <iostream>
 
-int main() {
-#if 1
-  TRACE("");
-  benchtools::WatchTimer t;
-  t.start();
-  std::cin.get();
-  t.stop();
-  benchtools::BaseTimer* cputim = new benchtools::CpuTimer();
+using namespace benchtools;
 
-  cputim->start();
-  INFO(t.timeElapsed().count());
-  /*
+int main() {
+  WatchTimer timer{};
   {
-    benchtools::ScopedTimer t2;
+    ScopedTimer scop(timer);
     std::cin.get();
-    }
-    */
-  std::cin.get();
-  cputim->stop();
-  INFO(cputim->timeElapsed().count());
-  delete cputim;
-#endif
+  }
+  std::clog << timer.duration(time_unit::nanoseconds).str() << std::endl;
 }
