4e93258000efeeb1a8cde0b51f774871ba365ad8
t <t.aryurek@student.utwente.nl>
2026-05-03 14:34:55 +0200

    Proper naming for sstream

--- src/Loggers/FileLogger.cpp
+++ src/Loggers/FileLogger.cpp
@@ -1,5 +1,5 @@
-#include "benchtools/Core/LogType.hpp"
 #include <benchtools/Core/FileStream.hpp>
+#include <benchtools/Core/LogType.hpp>
 #include <benchtools/Core/Time.hpp>
 #include <benchtools/Loggers/FileLogger.hpp>
 
@@ -11,21 +11,21 @@ namespace benchtools {
 FileLogger::FileLogger(std::string_view path) noexcept {
     static const char* s_start_message = "LOG STARTED";
     m_Stream = FileStream{path};
-    std::stringstream oss;
-    oss << format(time_date()) << format(LogType::INFO) << s_start_message << "\n";
-    m_Stream.append(oss.str());
+    std::stringstream ss;
+    ss << format(time_date()) << format(LogType::INFO) << s_start_message << "\n";
+    m_Stream.append(ss.str());
 }
 
 void FileLogger::Log(std::string_view content, LogType type) noexcept {
-    std::stringstream oss;
-    oss << format(time_date()) << format(type) << content << "\n";
-    m_Stream.append(oss.str());
-    oss.flush();
+    std::stringstream ss;
+    ss << format(time_date()) << format(type) << content << "\n";
+    m_Stream.append(ss.str());
+    ss.flush();
 }
 
 FileLogger::~FileLogger() noexcept {
-    std::stringstream oss;
-    oss << format(time_date()) << format(LogType::INFO) << "LOG END" << "\n";
-    m_Stream.append(oss.str());
+    std::stringstream ss;
+    ss << format(time_date()) << format(LogType::INFO) << "LOG END" << "\n";
+    m_Stream.append(ss.str());
 }
 };  // namespace benchtools
